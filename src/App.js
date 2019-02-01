c0daa09c21bbf799fa45a17cd90dd7c09fba6241
Denys Rebenok <denis.rebenok.pro@gmail.com>
2019-02-01 23:58:36 +0200

    install dependencies, refactor using eslint, convert Movie to function

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import Movie from "./Movie";
+import Movie from './Movie';
 
 class App extends Component {
   state = {
-    movies: []
+    movies: [],
   };
+
   async componentDidMount() {
     try {
       const res = await fetch(
-        "https://api.themoviedb.org/3/discover/movie?api_key=dff4354bf3abbf1784130401340e9e6f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1"
+        'https://api.themoviedb.org/3/discover/movie?api_key=dff4354bf3abbf1784130401340e9e6f&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1',
       );
       const movies = await res.json();
       this.setState({
-        movies: movies.results
+        movies: movies.results,
       });
     } catch (err) {
       console.log(err);
     }
   }
+
   render() {
+    const { movies } = this.state;
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {this.state.movies.map(movie => (
+        {movies.map(movie => (
           <Movie key={movie.id} movie={movie} />
         ))}
       </div>
