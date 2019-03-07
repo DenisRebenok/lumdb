440f06c44ea9b6165c172aa064d18ce3da3e7f19
Denys Rebenok <denis.rebenok.pro@gmail.com>
2019-03-07 21:36:21 +0200

    Dispatch Actions From Components

--- src/Toggle.js
+++ src/Toggle.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-const Toggle = ({ messageVisibility }) => {
+const Toggle = ({ messageVisibility, dispatch }) => {
   return (
     <div>
       {messageVisibility && (
         <p>You will be seeing this if redux action is toggled</p>
       )}
 
-      <button>Toggle Me</button>
+      <button
+        onClick={() =>
+          dispatch({
+            type: 'TOGGLE_MESSAGE'
+          })
+        }
+      >
+        Toggle Me
+      </button>
     </div>
   );
 };
